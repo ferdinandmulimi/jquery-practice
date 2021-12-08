e5ea05dc5574c6ca8876ab071c17f5a3fc5a5d9a
Ferdianand.mulimi <ferdinand.mulimi@student.moringaschool.com>
2021-12-08 14:17:12 +0300

    creation of gh-pages

--- jquery.js
+++ jquery.js
@@ -1,14 +1,15 @@
 // Analyse anatomy of the JQuery
 //Shortcuts replace - JQuery with $
-
-    jQuery("h1").click(function() {
-        alert("This is a header.")
+$(document).ready(function(){
+    $("h1").click(function() {
+       $("img").hide();
     });
 
-$("p").click(function () {
+    $("p").click(function () {
     alert("This is a paragraph.")
-});
+    });
 
-$("img").click(function () {
+    $("img").click(function () {
     alert("This is an image.")
-});
+    });
+})
