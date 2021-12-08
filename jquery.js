ef46cde354a47de7c69d152a64d20476bd39a7b7
Ferdianand.mulimi <ferdinand.mulimi@student.moringaschool.com>
2021-12-08 13:36:37 +0300

    to add the JQuery example

---
+++ jquery.js
@@ -0,0 +1,14 @@
+// Analyse anatomy of the JQuery
+//Shortcuts replace - JQuery with $
+
+    jQuery("h1").click(function() {
+        alert("This is a header.")
+    });
+
+$("p").click(function () {
+    alert("This is a paragraph.")
+});
+
+$("img").click(function () {
+    alert("This is an image.")
+});
